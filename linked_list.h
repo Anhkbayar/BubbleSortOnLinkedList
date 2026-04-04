673fc18ebcfb391fad93cbf66ffbbd8e5d113254
Enkhtaivan <enhtaivanbyambajav@gmail.com>
2026-04-04 17:31:35 +0800

    refactor, header file

--- linked_list.h
+++ linked_list.h
@@ -4,7 +4,7 @@
 
 //Node
 struct Node {
-    int   val;
+    int   data;
     Node* next;
 };
 
@@ -21,18 +21,47 @@ inline Node* createList(int n) {
     return head;
 }
 
-//Helpers
+/**
+ * deleteList: Frees every node in the linked list.
+ */
+inline void deleteList(Node* head) {
+    while (head) {
+        Node* nxt = head->next;
+        delete head;
+        head = nxt;
+    }
+}
+
+/**
+ * isSorted: Checks if the linked list is correctly sorted in ascending order.
+ */
+inline bool isSorted(Node* head, int n) {
+    if (!head) return (n == 0);
+    Node* cur = head;
+    int   cnt = 1;
+    while (cur->next) {
+        if (cur->data > cur->next->data) return false;
+        cur = cur->next;
+        ++cnt;
+    }
+    return (cnt == n);
+}
+
+/**
+ * printList: Utility to print the first 'limit' elements of the list.
+ */
 inline void printList(Node* head, int limit = 20) {
     int cnt = 0;
     for (Node* p = head; p && cnt < limit; p = p->next, ++cnt)
-        std::cout << p->val << (p->next && cnt + 1 < limit ? " -> " : "");
+        std::cout << p->data << (p->next && cnt + 1 < limit ? " -> " : "");
     std::cout << (limit && head ? " ...\n" : "\n");
 }
 
-inline void freeList(Node* head) {
-    while (head) { Node* t = head->next; delete head; head = t; }
-}
-
+/**
+ * listLength: Returns the number of nodes in the list.
+ */
 inline int listLength(Node* head) {
-    int n = 0; for (Node* p = head; p; p = p->next) ++n; return n;
+    int n = 0;
+    for (Node* p = head; p; p = p->next) ++n;
+    return n;
 }
